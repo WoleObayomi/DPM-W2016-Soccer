3bf2c446200035bbec5052332571abbc0f91524e
WoleObayomi <oluwole.obayomi@mail.mcgill.ca>
2016-03-22 16:43:59 -0400

    Updated javadoc with LauncherController class and updated class diagram

--- src/soccer/LauncherController.java
+++ src/soccer/LauncherController.java
@@ -13,6 +13,11 @@ import lejos.hardware.ev3.EV3;
 import lejos.hardware.motor.EV3LargeRegulatedMotor;
 import lejos.robotics.RegulatedMotor;
 
+/**
+ * 
+ * @author Peter Quinn
+ *
+ */
 public class LauncherController {
 
 	// motors
@@ -40,10 +45,19 @@ public class LauncherController {
 	private final int FIRE_ANGLE = 7;
 	private final int ANGLE_SPEED = 50;
 	private final int NUDGE_SPEED = 250;
-
-	public LauncherController(RegulatedMotor launcherRight, RegulatedMotor launcherLeft, RegulatedMotor angleAdjustMotor,
+	/**
+	 * 
+	 * @param launherRight
+	 * @param launcherLeft
+	 * @param conveyerRight
+	 * @param conveyerLeft
+	 * @param launcherWheelRadius
+	 * @param conveyerWheelRadius
+	 * @param ballDiameter
+	 */
+	public LauncherController(RegulatedMotor launherRight, RegulatedMotor launcherLeft, RegulatedMotor angleAdjustMotor,
 			EV3LargeRegulatedMotor conveyerRight, EV3LargeRegulatedMotor conveyerLeft, double launcherWheelRadius,
-			double conveyerWheelRadius, double ballDiameter) {
+		double conveyerWheelRadius, double ballDiameter) {
 		super();
 		this.launcherRight = launcherRight;
 		this.launcherLeft = launcherLeft;
@@ -54,9 +68,11 @@ public class LauncherController {
 		this.ballDiameter = ballDiameter;
 		this.angleAdjustMotor = angleAdjustMotor;
 	}
-
-	// stops launcher motors
-	public void stopLauncher() {
+	//stops launcher motors
+	/**
+	 * <p>This method stops the motors of the launcher
+	 */
+	public void stopLauncher(){
 		launcherLeft.setSpeed(0);
 		launcherRight.setSpeed(0);
 		launcherLeft.forward();
@@ -66,6 +82,10 @@ public class LauncherController {
 	}
 
 	// set launcher motors to firing speed
+	//set launcher motors to firing speed
+	/**
+	 * <p>sets the firing speed of the launcher
+	 */
 	public void setToFiringSpeed() {
 		launcherLeft.setSpeed(FIRING_SPEED);
 		launcherRight.setSpeed(FIRING_SPEED);
@@ -79,8 +99,11 @@ public class LauncherController {
 		}
 
 	}
-
-	public void setToIntakeSpeed() {
+	
+	/**
+	 * <p> Sets the motors to the speed required to retrieve a ball
+	 */
+	public void setToIntakeSpeed(){
 		launcherLeft.setSpeed(INTAKE_SPEED);
 		launcherRight.setSpeed(INTAKE_SPEED);
 		launcherLeft.setAcceleration(LAUNCHER_ACCELERATION);
@@ -92,8 +115,11 @@ public class LauncherController {
 		} catch (InterruptedException e) {
 		}
 	}
-
-	public void setToRejectSpeed() {
+	
+	/**
+	 *  <p> Sets the motors to the speed required to release an incorrect ball
+	 */
+	public void setToRejectSpeed(){
 		launcherLeft.setSpeed(REJECT_SPEED);
 		launcherRight.setSpeed(REJECT_SPEED);
 		launcherLeft.setAcceleration(LAUNCHER_ACCELERATION);
@@ -106,16 +132,22 @@ public class LauncherController {
 		}
 	}
 
-	public void conveyerBackOneBall() {
-		double theta = 360 * ballDiameter / (2 * Math.PI * conveyerWheelRadius) + CONVEYER_OFFSET;
+	/**
+	 *  <p> Moves the conveyor backwards by the size of a single ball
+	 */
+	public void conveyerBackOneBall(){
+		double theta = 360*ballDiameter/(2*Math.PI*conveyerWheelRadius)+CONVEYER_OFFSET;
 		conveyerLeft.setSpeed(CONVEYER_SPEED);
 		conveyerRight.setSpeed(CONVEYER_SPEED);
 		conveyerLeft.rotate((int) -theta, true);
 		conveyerRight.rotate((int) -theta, false);
 	}
 
-	public void conveyerForwardOneBall() {
-		double theta = 360 * ballDiameter / (2 * Math.PI * conveyerWheelRadius) + CONVEYER_OFFSET;
+	/**
+	 * <p> Moves the conveyor forwards by the size of a single ball
+	 */
+	public void conveyerForwardOneBall(){
+		double theta = 360*ballDiameter/(2*Math.PI*conveyerWheelRadius)+CONVEYER_OFFSET;
 		conveyerLeft.setSpeed(CONVEYER_SPEED);
 		conveyerRight.setSpeed(CONVEYER_SPEED);
 		conveyerLeft.rotate((int) theta, true);
--- src/soccer/ReversedEV3LargeMotor.java
+++ src/soccer/ReversedEV3LargeMotor.java
@@ -4,28 +4,51 @@ import lejos.hardware.motor.EV3LargeRegulatedMotor;
 import lejos.hardware.port.Port;
 
 //allows reversed motors to be used more intuitively 
+/**
+ * 
+ * @author Peter Quinn
+ *
+ */
 public class ReversedEV3LargeMotor extends EV3LargeRegulatedMotor{
 
+	/**
+	 * 
+	 * @param port
+	 */
 	public ReversedEV3LargeMotor(Port port) {
 		super(port);
 	}
 
+	/**
+	 * <p> Normalizes the forward movement (in the code) of the motor when orientation is reversed
+	 */
 	public void forward(){
 		super.backward();
 	}
 	
+	/**
+	 * <p> Normalizes the backward movement (in the code) of the motor when orientation is reversed
+	 */
 	public void backward(){
 		super.forward();
 	}
 	
+	/**
+	 * <p> Normalizes the tachometer count of the motor when orientation is reversed
+	 */
 	public int getTachoCount(){
 		return -super.getTachoCount();
 	}
 	
+	/**
+	 * <p> Normalizes the angle of rotation of the motor when orientation is reversed
+	 */
 	public void rotate(int angle){
 		super.rotate(-angle);
 	}
-	
+	/**
+	 * <p> Normalizes the forward movement of the motor when orientation is reversed and immediate return is desired
+	 */
 	public void rotate(int angle, boolean immediateReturn){
 		super.rotate(-angle, immediateReturn);
 	}
